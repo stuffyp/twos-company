b32bdaa27753157f6e5347f07a66d83f531a6a57
Eric Yang <eric@dhcp-10-31-91-27.dyn.mit.edu>
2022-12-18 15:37:06 -0500

    quality of life improvements

--- src/index.js
+++ src/index.js
@@ -146,16 +146,16 @@ class Game extends React.Component {
     }
 
     handleKey(event){
-        if(event.keyCode===37){
+        if(event.keyCode===38){
             if(this.state.stepNumber>0){
                 this.jumpTo(this.state.stepNumber-1);
             }
-        } else if(event.keyCode===39){
+        } else if(event.keyCode===40){
             if(this.state.stepNumber<this.state.history.length-1){
                 this.jumpTo(this.state.stepNumber+1);
             }
         }
-
+        event.preventDefault();
     }
 
     jumpTo(step) {
@@ -335,12 +335,13 @@ class Editor extends React.Component {
 
 class LevelSelect extends React.Component {
     constructor(props) {
-        const storedCompleted = JSON.parse(localStorage['completed']);
+        const storedCompleted = localStorage['completed'];
+        const completed = storedCompleted ? JSON.parse(storedCompleted) : Array(levels.numLevels()).fill(0);
         super(props);
         this.state = {
             level: 0,
             //elo: 700,
-            completed: storedCompleted ? storedCompleted : Array(levels.numLevels()).fill(0),
+            completed: completed,
             play: false,
             levelCode : levels.levelCode(0),
         };
@@ -412,6 +413,9 @@ class LevelSelect extends React.Component {
             <div className="levels-container">
                 {buttons}
                 <button className='level-button' onClick={() => this.setLevel(-1)}>{'Level Editor'}</button>
+            </div>
+            <div className='elo-container'>
+                <div>{this.state.level+1 ? 'Level '+ (this.state.level+1) + ':' : 'Level: Custom'}</div>
             </div>
             {/*<div className='elo-container'>
                 <div>{'ELO: '+ this.state.elo}</div>
